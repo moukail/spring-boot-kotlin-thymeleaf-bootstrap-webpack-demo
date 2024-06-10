84cf69d03d358a3247610558a053aceb0843056d
ismail Moukafih <ismail.moukafih@eviden.com>
2024-06-10 22:23:29 +0200

    webpack typescript bootstrap

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
+	id("com.github.node-gradle.node") version "7.0.2"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
 }
@@ -36,4 +37,25 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+node {
+	download.set(true)
+	version.set("20.13.1")
+}
+
+val npmRunBuild = tasks.register<com.github.gradle.node.npm.task.NpmTask>("npmRunBuild") {
+	args.set(listOf("run", "build"))
+
+	dependsOn(tasks.npmInstall)
+
+	inputs.files(fileTree("node_modules"))
+	inputs.files(fileTree("src/main/resources"))
+	inputs.file("package.json")
+	inputs.file("webpack.config.js")
+	outputs.dir(layout.buildDirectory.dir("resources/main/static"))
+}
+
+tasks.processResources {
+	dependsOn(npmRunBuild)
 }
